9f6d50fd917f9409ffa70ed02a823b0d9ddcef5a
Ludovic <leludo044@gmail.com>
2016-01-13 13:36:43 +0100

    Store database connection informations in GTRCHAMP_DATABASE environment variable

--- championship/mysql.js
+++ championship/mysql.js
@@ -1,45 +1,53 @@
 'use strict';
 
+var process = require("process");
+
 var dao = require("./dao");
 var DbConnector = require("./dbconnector");
 
-var connector = new DbConnector();
-connector.parse('mysql://root:root@localhost/gtrchamp?reconnect=true');
-console.log(connector);
+console.log('GTRCHAMP_DATABASE=' + process.env.GTRCHAMP_DATABASE);
+
+if (process.env.GTRCHAMP_DATABASE) {
+    var connector = new DbConnector();
+    connector.parse(process.env.GTRCHAMP_DATABASE);
+    console.log(connector);
 
 
-dao.connect(connector) ;
+    dao.connect(connector);
 
-dao.findAll(function (championships) {
-    console.log(championships);
-});
+    dao.findAll(function (championships) {
+        console.log(championships);
+    });
 
-dao.find(1, function (err, championship) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(championship);
-    }
-});
+    dao.find(1, function (err, championship) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(championship);
+        }
+    });
 
 
-dao.create({id:3, libelle:"toto"}, function(err, insertedId) {
-    console.log("Create "+insertedId);
-})
-dao.findAll(function (championships) {
-    console.log(championships);
-});
-dao.update({id:3, libelle:"titi"}, function(err, affectdRows) {
-    console.log("Update " + affectdRows);
-})
-dao.findAll(function (championships) {
-    console.log(championships);
-});
-dao.delete(3, function(err, affectdRows) {
-    console.log("Delete " + affectdRows);
-})
-dao.findAll(function (championships) {
-    console.log(championships);
-});
+    dao.create({ id: 3, libelle: "toto" }, function (err, insertedId) {
+        console.log("Create " + insertedId);
+    })
+    dao.findAll(function (championships) {
+        console.log(championships);
+    });
+    dao.update({ id: 3, libelle: "titi" }, function (err, affectdRows) {
+        console.log("Update " + affectdRows);
+    })
+    dao.findAll(function (championships) {
+        console.log(championships);
+    });
+    dao.delete(3, function (err, affectdRows) {
+        console.log("Delete " + affectdRows);
+    })
+    dao.findAll(function (championships) {
+        console.log(championships);
+    });
 
-dao.close();
+    dao.close();
+} else {
+    console.log("Variable d'environnement GTRCHAMP_DATABASE non définie");
+}
