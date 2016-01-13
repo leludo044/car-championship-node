7c107b2bc6aef8c253f7aca7f7c86fe86f76abc7
unknown <pmzn560@444150AS201.corp.log.intra.laposte.fr>
2016-01-13 16:42:04 +0100

    Refactoring DAO to class and improve return status codes for REST api

--- championship/dao.js
+++ championship/dao.js
@@ -3,96 +3,105 @@
 var mysql = require("mysql");
 var util = require("util");
 
-var checkId = function (id) {
-    var isValid = false;
-    var targetId = id;
-    if (util.isNumber(id)) {
-        isValid = true;
-    } else {
-        targetId = Number(id);
-        if (!isNaN(targetId)) {
-            isValid = true;
-        }
+class Dao {
+
+    constructor(dbConnector) {
+        this.connect(dbConnector);
     }
-    return isValid;
-};
 
-var connection ;
-var connect = function (dbConnector) {
-    connection = mysql.createConnection({
-        host: dbConnector.hostname,
-        user: dbConnector.username,
-        password: dbConnector.password,
-        database: dbConnector.database
-    });
-    connection.connect();
-};
-
-
-var findAll = function (callback) {
-    connection.query('select * from championnats',
-        function (err, rows, fields) {
-            if (!err) {
-                callback(rows);
-            } else {
-                console.log('Error while performing Query : ' + err);
+    checkId(id) {
+        let isValid = false;
+        let targetId = id;
+        if (util.isNumber(id)) {
+            isValid = true;
+        } else {
+            targetId = Number(id);
+            if (!isNaN(targetId)) {
+                isValid = true;
             }
-        });
-};
+        }
+        return isValid;
+    };
 
-var find = function (id, callback) {
-    if (checkId(id)) {
-        connection.query('select * from championnats where id=?', id,
+    connect(dbConnector) {
+        this.connection = mysql.createConnection({
+            host: dbConnector.hostname,
+            user: dbConnector.username,
+            password: dbConnector.password,
+            database: dbConnector.database
+        });
+        this.connection.connect();
+    };
+
+
+    findAll(callback) {
+        this.connection.query('select * from championnats',
             function (err, rows, fields) {
                 if (!err) {
-                    var error;
-                    if (rows.length === 0) {
-                        error = "Championship #" + id + " not found !";
-                    }
-                    callback(error, rows);
+                    callback(rows);
                 } else {
                     console.log('Error while performing Query : ' + err);
                 }
             });
-    } else {
-        var error = "Bad id format !";
-        callback(error, {});
+    };
+
+    find(id, callback) {
+        if (this.checkId(id)) {
+            this.connection.query('select * from championnats where id=?', id,
+                function (err, rows, fields) {
+                    if (!err) {
+                        var error;
+                        if (rows.length === 0) {
+                            error = "Championship #" + id + " not found !";
+                        }
+                        callback(error, rows);
+                    } else {
+                        console.log('Error while performing Query : ' + err);
+                    }
+                });
+        } else {
+            var error = "Bad id format !";
+            callback(error, {});
+        }
+    };
+
+    create(championship, callback) {
+        this.connection.query('insert into championnats set ?', championship,
+            function (err, result) {
+                if (err) {
+                    console.log(err);
+                }
+                callback(err, result.insertId);
+            });
+    };
+
+    update(championship, callback) {
+        this.connection.query('update championnats set libelle=? where id=?', [championship.libelle, championship.id],
+            function (err, result) {
+                if (!err) {
+                    if (result.affectedRows == 0) {
+                        err = "Championship #" + championship.id + " not updated !";
+                    }
+                }
+                callback(err, result.affectedRows);
+            });
+    };
+
+    delete(id, callback) {
+        this.connection.query('delete from championnats where id=?', id,
+            function (err, result) {
+                if (!err) {
+                    if (result.affectedRows == 0) {
+                        err = "Championship #" + id + " not deleted !";
+                    }
+                }
+                callback(err, result.affectedRows);
+            });
+    };
+
+    close() {
+        this.connection.end();
     }
-};
+}
 
-module.exports.create = function (championship, callback) {
-    connection.query('insert into championnats set ?', championship,
-        function (err, result) {
-            if (err) {
-                console.log(err);
-            }
-            callback(err, result.insertId);
-        });
-};
-
-module.exports.update = function (championship, callback) {
-    connection.query('update championnats set libelle=? where id=?', [championship.libelle, championship.id],
-        function (err, result) {
-            if (err) {
-                console.log(err);
-            }
-            callback(err, result.affectedRows);
-        });
-};
-
-module.exports.delete = function (id, callback) {
-    connection.query('delete from championnats where id=?', id,
-        function (err, result) {
-            if (err) {
-                console.log(err);
-            }
-            callback(err, result.affectedRows);
-        });
-};
-
-module.exports.findAll = findAll;
-module.exports.find = find;
-module.exports.close = function () {
-    connection.end();
-};
-module.exports.connect = connect ;
+module.exports = Dao;
--- championship/expressserver.js
+++ championship/expressserver.js
@@ -2,56 +2,63 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var process = require("process");
 
-var dao = require('./dao');
+var Dao = require('./dao');
 var DbConnector = require("./dbconnector");
+
 
 var connector = new DbConnector();
 connector.parse(process.env.GTRCHAMP_DATABASE);
 console.log(connector);
-dao.connect(connector);
 
+var dao = new Dao(connector);
 
-var app = express() ; 
+var app = express();
 
 app.use(bodyParser.json());
 
-app.get('/championships', function(request, response) {
-		dao.findAll(function (championships) {
-			response.json(championships);
-		});
-	}); 
-	
-app.get('/championships/:id', function(request, response) {
-		dao.find(request.params.id, function (error, championship) {
-			if (!error) {
-                response.json(championship);
-			} else {
-                response.status(404).json({ "message": error });
-			}
-		});
-	});
-
-app.put('/championships', function(request, response) {
-    console.log(request.body);
-    dao.update(request.body, function(err, insertedId){
-        console.log(insertedId);
+app.get('/championships', function (request, response) {
+    dao.findAll(function (championships) {
+        response.json(championships);
     });
-    response.end() ;
 });
 
-app.post('/championships', function(request, response) {
-    console.log(request.body);
-    dao.create(request.body, function(err, insertedId){
-        console.log(insertedId);
+app.get('/championships/:id', function (request, response) {
+    dao.find(request.params.id, function (error, championship) {
+        if (!error) {
+            response.json(championship);
+        } else {
+            response.status(404).json({ "message": error });
+        }
     });
-    response.end() ;
 });
 
-app.delete('/championships/:id', function(request, response) {
-    dao.delete(request.params.id, function(err, affectdRows) {
-        console.log(affectdRows);
-    })    
-    response.end() ;
+app.put('/championships', function (request, response) {
+    console.log(request.body);
+    dao.update(request.body, function (err, insertedId) {
+        if (err) {
+            response.status(404).json({ "message": err });
+        } else {
+            response.end();
+        }
+    });
+});
+
+app.post('/championships', function (request, response) {
+    console.log(request.body);
+    dao.create(request.body, function (err, insertedId) {
+        console.log(insertedId);
+    });
+    response.end();
+});
+
+app.delete('/championships/:id', function (request, response) {
+    dao.delete(request.params.id, function (err, affectdRows) {
+        if (err) {
+            response.status(404).json({ "message": err });
+        } else {
+            response.end();
+        }
+    })
 });
 
 app.listen(3000);
