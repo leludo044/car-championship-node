0c15f8dfe4cd7f058d212e93bda098cd12756484
Ludovic <leludo044@gmail.com>
2016-01-13 16:06:48 +0100

    Refactoring express api usage

--- championship/expressserver.js
+++ championship/expressserver.js
@@ -16,22 +16,17 @@ var app = express() ;
 app.use(bodyParser.json());
 
 app.get('/championships', function(request, response) {
-		response.setHeader('Content-Type', 'application/json');
 		dao.findAll(function (championships) {
-			response.send(JSON.stringify(championships));
+			response.json(championships);
 		});
 	}); 
 	
 app.get('/championships/:id', function(request, response) {
 		dao.find(request.params.id, function (error, championship) {
 			if (!error) {
-				response.writeHead(200, { "Content-Type": "application/json" });
-				response.write(JSON.stringify(championship));
-				response.end();
+                response.json(championship);
 			} else {
-				response.writeHead(404, { "Content-Type": "application/json" });
-				response.write(JSON.stringify({ "message": error }));
-				response.end();
+                response.status(404).json({ "message": error });
 			}
 		});
 	});
