219dbfd05e3d345efd346359e43df638141ada72
unknown <pmzn560@444150AS201.corp.log.intra.laposte.fr>
2016-01-14 13:44:50 +0100

    Dao refactoring

--- championship/championship-dao.js
+++ championship/championship-dao.js
@@ -2,80 +2,13 @@
 
 var mysql = require("mysql");
 var util = require("util");
+var DefaultDao = require('./default-dao');
 
-class Dao {
+class ChampionshipDao extends DefaultDao {
 
     constructor(dbConnector) {
-        this.connect(dbConnector);
+        super(dbConnector, 'championnats', 'Championship');
     }
-
-    checkId(id) {
-        let isValid = false;
-        let targetId = id;
-        if (util.isNumber(id)) {
-            isValid = true;
-        } else {
-            targetId = Number(id);
-            if (!isNaN(targetId)) {
-                isValid = true;
-            }
-        }
-        return isValid;
-    };
-
-    connect(dbConnector) {
-        this.connection = mysql.createConnection({
-            host: dbConnector.hostname,
-            user: dbConnector.username,
-            password: dbConnector.password,
-            database: dbConnector.database
-        });
-        this.connection.connect();
-    };
-
-
-    findAll(callback) {
-        this.connection.query('select * from championnats',
-            function (err, rows, fields) {
-                if (!err) {
-                    callback(rows);
-                } else {
-                    console.log('Error while performing Query : ' + err);
-                }
-            });
-    };
-
-    find(id, callback) {
-        if (this.checkId(id)) {
-            this.connection.query('select * from championnats where id=?', id,
-                function (err, rows, fields) {
-                    if (!err) {
-                        var error;
-                        if (rows.length === 0) {
-                            error = "Championship #" + id + " not found !";
-                        }
-                        callback(error, rows);
-                    } else {
-                        console.log('Error while performing Query : ' + err);
-                    }
-                });
-        } else {
-            var error = "Bad id format !";
-            callback(error, {});
-        }
-    };
-
-    create(championship, callback) {
-        this.connection.query('insert into championnats set ?', championship,
-            function (err, result) {
-                if (err) {
-                    console.log(err);
-                    callback(err, -1) ;
-                } else {
-                    callback(err, result.insertId);
-                }
-            });
-    };
 
     update(championship, callback) {
         this.connection.query('update championnats set libelle=? where id=?', [championship.libelle, championship.id],
@@ -88,22 +21,6 @@ class Dao {
                 callback(err, result.affectedRows);
             });
     };
-
-    delete(id, callback) {
-        this.connection.query('delete from championnats where id=?', id,
-            function (err, result) {
-                if (!err) {
-                    if (result.affectedRows == 0) {
-                        err = "Championship #" + id + " not deleted !";
-                    }
-                }
-                callback(err, result.affectedRows);
-            });
-    };
-
-    close() {
-        this.connection.end();
-    }
 }
 
-module.exports = Dao;
+module.exports = ChampionshipDao;
---
+++ championship/default-dao.js
@@ -0,0 +1,98 @@
+"use strict";
+
+var mysql = require("mysql");
+var util = require("util");
+
+class DefaultDao {
+
+    constructor(dbConnector, tableName, entityName) {
+        this.connect(dbConnector);
+        this.tableName = tableName;
+        this.entityName = entityName;
+    }
+
+    checkId(id) {
+        let isValid = false;
+        let targetId = id;
+        if (util.isNumber(id)) {
+            isValid = true;
+        } else {
+            targetId = Number(id);
+            if (!isNaN(targetId)) {
+                isValid = true;
+            }
+        }
+        return isValid;
+    };
+
+    connect(dbConnector) {
+        this.connection = mysql.createConnection({
+            host: dbConnector.hostname,
+            user: dbConnector.username,
+            password: dbConnector.password,
+            database: dbConnector.database
+        });
+        this.connection.connect();
+    };
+
+    close() {
+        this.connection.end();
+    }
+
+    findAll(callback) {
+        this.connection.query('select * from ' + this.tableName,
+            function (err, rows, fields) {
+                if (!err) {
+                    callback(rows);
+                } else {
+                    console.log('Error while performing Query : ' + err);
+                }
+            });
+    };
+
+    find(id, callback) {
+        if (this.checkId(id)) {
+            this.connection.query('select * from ' + this.tableName + ' where id=?', id,
+                function (err, rows, fields) {
+                    if (!err) {
+                        var error;
+                        if (rows.length === 0) {
+                            error = this.entityName + " #" + id + " not found !";
+                        }
+                        callback(error, rows);
+                    } else {
+                        console.log('Error while performing Query : ' + err);
+                    }
+                });
+        } else {
+            var error = "Bad id format !";
+            callback(error, {});
+        }
+    };
+
+    create(entity, callback) {
+        this.connection.query('insert into ' + this.tableName + ' set ?', entity,
+            function (err, result) {
+                if (err) {
+                    console.log(err);
+                    callback(err, -1);
+                } else {
+                    callback(err, result.insertId);
+                }
+            });
+    };
+
+    delete(id, callback) {
+        this.connection.query('delete from ' + this.tableName + ' where id=?', id,
+            function (err, result) {
+                if (!err) {
+                    if (result.affectedRows == 0) {
+                        err = this.entityName + " #" + id + " not deleted !";
+                    }
+                }
+                callback(err, result.affectedRows);
+            });
+    };
+}
+
+module.exports = DefaultDao;
