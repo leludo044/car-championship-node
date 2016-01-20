d0e4906ee46fd3b968dfdf570985cd8d709450b7
unknown <pmzn560@444150AS201.corp.log.intra.laposte.fr>
2016-01-20 17:58:24 +0100

    Country dao and router implementation

---
+++ championship/country-dao.js
@@ -0,0 +1,131 @@
+"use strict";
+
+var mysql = require("mysql");
+var util = require("util");
+
+class DefaultDao {
+
+    constructor(dbConnector, tableName, entityName) {
+        this.connect(dbConnector);
+        this.tableName = tableName;
+        this.entityName = entityName;
+    }
+
+    checkId(id) {
+        let isValid = false;
+        let targetId = id;
+        if (util.isNumber(id)) {
+            isValid = true;
+        } else {
+            targetId = Number(id);
+            if (!isNaN(targetId)) {
+                isValid = true;
+            }
+        }
+        return isValid;
+    };
+
+    connect(dbConnector) {
+        this.connection = mysql.createConnection({
+            host: dbConnector.hostname,
+            user: dbConnector.username,
+            password: dbConnector.password,
+            database: dbConnector.database
+        });
+        this.connection.connect();
+    };
+
+    close() {
+        this.connection.end();
+    }
+
+    findAll(callback) {
+        this.connection.query('select * from ' + this.tableName,
+            function (err, rows, fields) {
+                if (!err) {
+                    callback(rows);
+                } else {
+                    console.log('Error while performing Query : ' + err);
+                }
+            });
+    };
+
+    find(id, callback) {
+        if (this.checkId(id)) {
+            this.connection.query('select * from ' + this.tableName + ' where id=?', id,
+                function (err, rows, fields) {
+                    if (!err) {
+                        var error;
+                        if (rows.length === 0) {
+                            error = this.entityName + " #" + id + " not found !";
+                        }
+                        callback(error, rows);
+                    } else {
+                        console.log('Error while performing Query : ' + err);
+                    }
+                });
+        } else {
+            var error = "Bad id format !";
+            callback(error, {});
+        }
+    };
+
+    create(entity, callback) {
+        this.connection.query('insert into ' + this.tableName + ' set ?', entity,
+            function (err, result) {
+                if (err) {
+                    console.log(err);
+                    callback(err, -1);
+                } else {
+                    callback(err, result.insertId);
+                }
+            });
+    };
+
+    update(entity, callback) {
+        let query = 'update ' + this.tableName + ' set ';
+        let params = [];
+        let self = this ;
+
+        let more = false;
+        this.updateStategy.forEach(function (element) {
+            if (more) {
+                query += ', '
+            }
+            query += (element + '=? ');
+            params.push(entity[element]);
+            more = true ;
+        }, this);
+        query += 'where id=?';
+
+        params.push(entity.id);
+
+        console.log(query);
+        console.log(params);
+        this.connection.query(query, params,
+            function (err, result) {
+                if (!err) {
+                    if (result.affectedRows == 0) {
+                        err = self.entityName + " #" + entity.id + " not updated !";
+                    }
+                }
+                callback(err, result.affectedRows);
+            });
+    };
+    
+    delete(id, callback) {
+        let self = this ;
+
+        this.connection.query('delete from ' + this.tableName + ' where id=?', id,
+            function (err, result) {
+                if (!err) {
+                    if (result.affectedRows == 0) {
+                        err = self.entityName + " #" + id + " not deleted !";
+                    }
+                }
+                callback(err, result.affectedRows);
+            });
+    };
+}
+
+module.exports = DefaultDao;
--- championship/expressserver.js
+++ championship/expressserver.js
@@ -5,10 +5,12 @@ var process = require("process");
 var ChampionshipDao = require('./championship-dao');
 var DriverDao = require('./driver-dao');
 var TrackDao = require('./track-dao');
+var CountryDao = require('./country-dao'); 
 var DbConnector = require("./dbconnector");
 var championshipRouter = require("./default-router");
 var driverRouter = require("./default-router");
 var trackRouter = require("./default-router");
+var countryRouter = require("./default-router");
 
 var connector = new DbConnector();
 connector.parse(process.env.GTRCHAMP_DATABASE);
@@ -23,9 +25,13 @@ driverRouter.dao(driverDao);
 var trackDao = new TrackDao(connector);
 trackRouter.dao(trackDao);
 
+var countryDao = new CountryDao(connector, "pays", "Pays");
+countryRouter.dao(countryDao);
+
 var app = express();
 app.use(bodyParser.json());
 app.use('/championship', championshipRouter);
 app.use('/driver', driverRouter);
 app.use('/track', trackRouter);
+app.use('/country', countryRouter);
 app.listen(3000);
